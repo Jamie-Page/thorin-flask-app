74915957b5c0559aff58939c3b3350bf2248c03a
Jamie-Page <jamie_page1998@hotmail.com>
2021-12-14 16:09:32 +0000

    added templated folder

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
-
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
 @app.route("/")
-    return "Hello, World"
+def index():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
