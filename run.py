acbbab5f4c170eb874553f6d87ff99f642bb609c
Jamie-Page <jamie_page1998@hotmail.com>
2021-12-17 14:17:25 +0000

    changed run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+
 import os
 from flask import Flask, render_template
 
@@ -18,6 +19,11 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
 
 
 if __name__ == "__main__":
